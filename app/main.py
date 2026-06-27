857c319971ec67264b5130af8df7723b364eb28d
Maciek Zaweracz <mz1794@york.ac.uk>
2026-06-27 11:40:42 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -16,8 +16,17 @@ def handle_command(cmd):
     match cmd.split()[0]:
         case "echo":
             print(cmd[5:])
+        case "type":
+            arg = cmd[5:]
+            match arg:
+                case "echo" | "exit":
+                    print(f"{arg} is a shell builtin")
+                case _:
+                    print(f"{arg}: not found")
+
         case _:
             sys.stdout.write(f"{cmd}: command not found\n")
+
 
 if __name__ == "__main__":
     main()
