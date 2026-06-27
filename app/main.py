afa7e3c7f664d33dda35d719499246441e143e0a
Maciek Zaweracz <mz1794@york.ac.uk>
2026-06-27 11:32:46 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,16 @@ def main():
         user_input = sys.stdin.readline().rstrip()
         if user_input == "exit":
             break
-        sys.stdout.write(f"{user_input}: command not found\n")
+
+        handle_command(user_input)
+
+
+def handle_command(cmd):
+    match cmd.split()[0]:
+        case "echo":
+            print(cmd[4:])
+        case _:
+            sys.stdout.write(f"{cmd}: command not found\n")
 
 if __name__ == "__main__":
     main()
