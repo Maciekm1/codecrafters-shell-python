788f70644542dab48b722b17df3d2e8620bb6310
Maciek Zaweracz <mz1794@york.ac.uk>
2026-06-27 11:23:12 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import sys
 
-
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while 1:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    user_input = sys.stdin.readline().rstrip()
-    sys.stdout.write(f"{user_input}: command not found\n")
+        user_input = sys.stdin.readline().rstrip()
+        sys.stdout.write(f"{user_input}: command not found\n")
 
 if __name__ == "__main__":
     main()
